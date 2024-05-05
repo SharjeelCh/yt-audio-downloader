ec940ba4fe213c4aa08e848dc2c3f3139544a566
SharjeelCh <sharjeelh6451@gmail.com>
2024-05-05 16:34:05 +0500

    responsivness enchanced

--- src/App.js
+++ src/App.js
@@ -54,27 +54,27 @@ function App() {
   };
 
   return (
-    <div className="h-screen flex-wrap">
+    <div className="h-screen flex flex-col w-full">
       <form
         action="/convert-mp3"
         method="POST"
         id="form"
         onSubmit={handleSubmit}
-        className="w-screen flex flex-col justify-center items-center bg-green-600 h-64"
+        className="w-screen flex flex-col justify-center items-center bg-green-600 h-64 py-4 sm:py-16 md:py-20 pl-2 sm:pl-3 md:pl-4"
       >
-        <h1 className="font-espn text-4xl text-white italic font-bold pt-36">
-          <FontAwesomeIcon icon={faYoutube} className="text-white-500 h-10" />{" "}
+        <h1 className="font-espn text-4xl text-white italic font-bold pt-36 sm:text-5xl md:text-5xl">
+          <FontAwesomeIcon icon={faYoutube} className="text-white-500 h-12 sm:h-10 md:h-12" />{" "}
           YouTube to MP3 Converter
         </h1>
-        <h4 className="font-espn text-lg text-white py-5">
+        <h4 className="font-espn text-lg text-white py-3 sm:text-xl py-3 sm:py-4 md:py-5">
           Enter the Youtube video ID
         </h4>
-        <div className="bg-white w-[350px] h-[200px] flex flex-row justify-end align-end rounded-lg shadow-sm overflow-visible">
+        <div className="bg-white w-72 sm:w-80 md:w-96 ml-3 mr-3 flex flex-row justify-end align-end rounded-lg shadow-sm overflow-visible">
           <input
             name="videoID"
             type="text"
             placeholder="Enter video ID"
-            className="ml-2 border-none w-64"
+            className="flex-grow ml-2 border-none w-64 "
             style={{height:'100%'}}
           ></input>
           <button
@@ -89,16 +89,16 @@ function App() {
       </form>
 
       {error ? (
-        <div className="w-screen items-center flex flex-col mt-16 justify-center align-center">
+        <div className="w-screen items-center flex flex-col mt-10 sm:mt-12 md:mt-16 px-3 sm:px-4 md:px-4 justify-center align-center">
           <p className={serror}>{e}</p>
         </div>
       ) : (
-        <div className="w-screen items-center flex flex-col mt-16 justify-center align-center">
+        <div className="w-screen items-center flex flex-col mt-10 sm:mt-12 md:mt-16 justify-center align-center px-3 sm:px-4 md:px-4">
           <p className="font-espn text-black tracking-widest">{title}</p>
           <a href={link} download>
             <button
               id="download_btn"
-              className="w-40 h-10 mt-3 tracking-widest rounded-lg bg-green-600 font-espn text-white hover:text-lg"
+              className="w-32 sm:w-36 md:w-40 h-8 sm:h-10 mt-3 tracking-widest rounded-lg bg-green-600 font-espn text-white hover:text-lg"
             >
               Download
             </button>
